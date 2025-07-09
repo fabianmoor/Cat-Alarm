4c09a56dc740a613ef159f2c578bbb9cd912ca5b
fabianmoor <fabbe142@gmail.com>
2025-07-09 11:13:54 +0200

    Simpler sensors method

--- sensors.py
+++ sensors.py
@@ -1,47 +1,27 @@
-from machine import Pin
-from utime import sleep_us, ticks_us, ticks_diff
-from config import ULTRASONIC_TIMEOUT_US
+from machine import Pin, time_pulse_us
+from utime import sleep_us
 
-# class for the ultrasonic sensor
+SOUND_SPEED_CM_PER_US = 0.0343
+TRIG_PULSE_DURATION_US = 10
+
 class UltrasonicSensor:
     def __init__(self, trigger_pin, echo_pin):
-        # setup the pins
         self.trigger = Pin(trigger_pin, Pin.OUT)
         self.echo = Pin(echo_pin, Pin.IN)
-
-    # function to get distance in cm
-    def get_distance_cm(self):
-        # send a pulse
-        self.trigger.low()
-        sleep_us(2)
-        self.trigger.high()
+        self.trigger.value(0)
         sleep_us(5)
-        self.trigger.low()
 
-        # wait for echo to start
-        start_time_high = ticks_us()
-        pulse_start = None
-        while self.echo.value() == 0:
-            pulse_start = ticks_us()
-            # timeout if taking too long
-            if ticks_diff(ticks_us(), start_time_high) > ULTRASONIC_TIMEOUT_US:
-                return -1
+    def get_distance_cm(self):
+        self.trigger.value(1)
+        sleep_us(TRIG_PULSE_DURATION_US)
+        self.trigger.value(0)
 
-        # wait for echo to end
-        start_time_low = ticks_us()
-        pulse_end = None
-        while self.echo.value() == 1:
-            pulse_end = ticks_us()
-            # timeout if taking too long
-            if ticks_diff(ticks_us(), start_time_low) > ULTRASONIC_TIMEOUT_US:
-                return -1
+        pulse_duration_us = time_pulse_us(self.echo, 1, 30000)
 
-        # check if we got valid readings
-        if pulse_start is None or pulse_end is None:
+        if pulse_duration_us < 0:
             return -1
 
-        # calculate distance using time
-        time_passed = ticks_diff(pulse_end, pulse_start)
-        distance = (time_passed * 0.0343) / 2
-        print("The distance from object is ", round(distance, 2), "cm")
+        distance = (pulse_duration_us * SOUND_SPEED_CM_PER_US) / 2
+
+        print("The distance from object is {:.2f} cm".format(distance))
         return distance
